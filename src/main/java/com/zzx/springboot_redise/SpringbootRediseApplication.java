b5f0aa715247a142d166922c0aa2f283704c4a6d
John <1101648204@qq.com>
2018-11-30 16:09:00 +0800

    redis工具类

--- src/main/java/com/zzx/springboot_redise/SpringbootRediseApplication.java
+++ src/main/java/com/zzx/springboot_redise/SpringbootRediseApplication.java
@@ -1,12 +1,8 @@
 package com.zzx.springboot_redise;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration;
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.data.redis.cache.RedisCacheManager;
 
 //@EnableAutoConfiguration(exclude = {RedisAutoConfiguration.class})
 @SpringBootApplication
